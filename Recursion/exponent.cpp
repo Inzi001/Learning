7520cefcdb8a4abcacd90b79320fec3c7bb4cdd2
inzi001 <inzamamul9525@gmail.com>
2023-09-21 12:33:40 +0530

    commit

---
+++ Recursion/exponent.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int power(int a, int b){
+    if (b==0)
+        return 1;
+    if(b%2 == 0)
+        return power(a*a, b/2);
+    else
+        return a * power(a * a, b/2);
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int ans = power(a,b);
+    cout<<ans;
+    return 0;
+}
